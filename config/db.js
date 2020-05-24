dee137acd2e5f1cd0f9108c3ffac9308b564e2db
therahulmehta <rahul95mehta@gmail.com>
2020-05-24 16:48:59 -0400

    connecting mongoDB

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+//returns promise instead of then and catch, async code.
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    //exit process with fail
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
