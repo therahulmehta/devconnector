b795b3bcfe5e6758eae30f0d724f46c31ed0a2b6
therahulmehta <rahul95mehta@gmail.com>
2020-05-25 09:33:57 -0400

    vaidtion for user object

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// https://express-validator.github.io/docs/
-const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator"); // https://express-validator.github.io/docs/
+const User = require("../../models/User");
 
 // @route  GET api/users
 // @desc   Test route
@@ -15,13 +17,51 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // 400 is bad request
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      // see if user exists
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //encrypt password
+      const salt = await bcrypt.genSalt(10); //hashing variable
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save(); // await on anything that return promise
+
+      //return jsonwebtoken; auto login after registering
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 module.exports = router;
