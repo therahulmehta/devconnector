57c2c2d19e44d40cba4576ffe974c92806bd332f
therahulmehta <rahul95mehta@gmail.com>
2020-05-25 10:08:24 -0400

    tested token handling with middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route  POST api/auth
-// @desc   Register user
-// @access  public
-router.get("/", (req, res) => res.send("Auth route"));
+// @route  GET api/auth
+// @desc   Test route
+// @access Public
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Sever error!");
+  }
+});
 
 module.exports = router;
