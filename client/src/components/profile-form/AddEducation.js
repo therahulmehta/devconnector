bbd7e16f3392224a0f139e6057342ceb34d2687e
therahulmehta <rahul95mehta@gmail.com>
2020-06-03 18:34:42 -0400

    hotfix for react hook warning

--- client/src/components/profile-form/AddEducation.js
+++ client/src/components/profile-form/AddEducation.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
-// import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { addEducation } from "../../actions/profile";
 
@@ -129,4 +129,4 @@ AddEducation.propTypes = {
   addEducation: PropTypes.func.isRequired,
 };
 
-export default connect(null, { addEducation })(AddEducation);
+export default connect(null, { addEducation })(withRouter(AddEducation));
