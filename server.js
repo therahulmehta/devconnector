5789a093804e68d220f73f08203f1fed720135e2
therahulmehta <rahul95mehta@gmail.com>
2020-06-10 14:16:10 -0400

    pre production code: comment form issue

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require("path");
 const app = express();
 
 connectDB();
 
 // init middleware
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes api/users -> '/'
 // End points are restful
@@ -17,6 +15,14 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // watch for express port environment variable or defualt to local port
 const PORT = process.env.PORT || 5000;
 
